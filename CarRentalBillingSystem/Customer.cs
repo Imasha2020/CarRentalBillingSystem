17f4c728e8dd0f5a7a85738cb88c8f52aa80bc03
Imasha <imashaudayanganie2020@gmail.com>
2025-09-13 19:17:55 +0530

    created Customer , CustomerList classes and Rental interface

---
+++ CarRentalBillingSystem/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentalBillingSystem
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string VehicleType { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public double FinalBill { get; set; }
+    }
+}
